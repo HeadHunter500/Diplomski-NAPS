44c35dec665a1e0b4b310f28646eb35181f31c76
HeadHunter500 <matko1993bj@gmail.com>
2017-11-07 17:22:42 +0100

    User to database, PHP scripts
    - Adding user to online mysql database
    - Added folder with PHP scripts for connecting to database

--- app/src/main/java/com/tvz/matko/naps/UserActivity.java
+++ app/src/main/java/com/tvz/matko/naps/UserActivity.java
@@ -1,11 +1,14 @@
 package com.tvz.matko.naps;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -16,17 +19,54 @@ import android.widget.RadioButton;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONObject;
+
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.io.*;
+
+import android.provider.Settings.Secure;
+import android.content.Context;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.lang.Object;
 
 public class UserActivity extends AppCompatActivity {
+
+
 
     EditText name;
     Spinner age;
     Spinner sex;
-    //RadioButton male;
-    //RadioButton female;
+
     Button start;
+
+    String TempName;
+    Integer TempAge;
+    Integer TempSex;
+
+    // url to create new product
+    private static String ServerURL = "http://lqovz8nye-site.etempurl.com/scripts/add_user.php";
+
+    String line = null;
+    int code;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,76 +74,83 @@ public class UserActivity extends AppCompatActivity {
 
         //fullscreen, removes upper status bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         setContentView(R.layout.activity_user);
+
+        //EditText
+        name = (EditText) findViewById(R.id.editTextName);
+        //Spinners
+        age = (Spinner) findViewById(R.id.spinnerAge);
+        sex = (Spinner) findViewById(R.id.spinnerSex);
+        //Button
+        start = (Button) findViewById(R.id.buttonStartUser);
+
 
 
         //Populate spinner with numbers
         List ageList = new ArrayList<Integer>();
-        ageList.add(0,"Age");
+        ageList.add(0, "Age");
         for (int i = 1; i <= 100; i++) {
             ageList.add(Integer.toString(i));
         }
+
         ArrayAdapter<Integer> spinnerArrayAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, ageList);
-        spinnerArrayAdapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //adding years to age spinner
-        age = (Spinner)findViewById(R.id.spinnerAge);
         age.setAdapter(spinnerArrayAdapter);
+
+
+
 
         //Populate spinner with genders
         List sexList = new ArrayList();
-        sexList.add(0,"Gender");
-        sexList.add(1,"Male");
-        sexList.add(2,"Female");
+        sexList.add(0, "Gender");
+        sexList.add(1, "Male");
+        sexList.add(2, "Female");
 
         ArrayAdapter<Integer> spinnerGenderAdapter = new ArrayAdapter<Integer>(this, android.R.layout.simple_spinner_item, sexList);
-        spinnerGenderAdapter.setDropDownViewResource( android.R.layout.simple_spinner_dropdown_item );
+        spinnerGenderAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         //adding years to age spinner
-        sex = (Spinner)findViewById(R.id.spinnerSex);
         sex.setAdapter(spinnerGenderAdapter);
 
 
-        start = (Button) findViewById(R.id.buttonStartUser);
-        name = (EditText) findViewById(R.id.editTextName);
 
-        sex = (Spinner)findViewById(R.id.spinnerSex);
 
-        //male = (RadioButton) findViewById(R.id.radioButtonMale);
-        //female = (RadioButton) findViewById(R.id.radioButtonFemale);
+
 
 
         //When user presses start button app checks if user provided all of the info
-        start.setOnClickListener(new View.OnClickListener(){
-            public void onClick(View v){
+        start.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                GetData();
 
                 //Checking network connection
-                if(!isNetworkAvailable())
-                {
+                if (!isNetworkAvailable()) {
                     Toast.makeText(UserActivity.this, R.string.no_connection, Toast.LENGTH_LONG).show();
 
                 }
                 //Checking if Name is entered
-                else if(name.getText().toString().equals("")){
+                else if (TempName.equals("")) {
                     Toast.makeText(UserActivity.this, R.string.empty_name, Toast.LENGTH_LONG).show();
                 }
 
                 //Checking if Age is entered
-                else if(age.getItemAtPosition(age.getSelectedItemPosition()).toString().equals("Age")){
+                else if (age.getItemAtPosition(TempAge).toString().equals("Age")) {
                     Toast.makeText(UserActivity.this, R.string.empty_age, Toast.LENGTH_LONG).show();
                 }
 
                 //Checking if Age is entered
-                else if(sex.getItemAtPosition(sex.getSelectedItemPosition()).toString().equals("Gender")){
+                else if (sex.getItemAtPosition(TempSex).toString().equals("Gender")) {
                     Toast.makeText(UserActivity.this, R.string.empty_sex, Toast.LENGTH_LONG).show();
+                } else {
+                    //When button Start is clicked, go to Picture
+
+                  new Prijava().execute();
+
                 }
-
-                else{
-                    //When button About is clicked, go to About activity
-                    startActivity(new Intent(UserActivity.this, PictureActivity.class));
-
-                }
-
 
             }
         });
@@ -111,6 +158,105 @@ public class UserActivity extends AppCompatActivity {
 
     }
 
+    public void GetData(){
+
+        TempName = name.getText().toString();
+
+        TempAge = age.getSelectedItemPosition();
+
+        TempSex = sex.getSelectedItemPosition();
+
+    }
+
+//_________________________________________
+
+    class Prijava extends AsyncTask<String, String, Void> {
+
+        InputStream is = null;
+        String result = "";
+
+        protected void onPreExecute() {
+        }
+
+        @Override
+        //Spajanje na skriptu koja se spaja na tablicu korisnika i izlistava ih
+        protected Void doInBackground(String... params) {
+
+            ArrayList<NameValuePair> NameValuePairs = new ArrayList<NameValuePair>();
+
+            NameValuePairs.add(new BasicNameValuePair("name", TempName));
+            NameValuePairs.add(new BasicNameValuePair("age", TempAge.toString()));
+            NameValuePairs.add(new BasicNameValuePair("sex", TempSex.toString()));
+
+            try {
+
+                //Spajanje na server gdje se nalazi skripta koja unosi korisnika u bazu podataka
+                HttpClient httpclient = new DefaultHttpClient();
+                HttpPost httppost = new HttpPost(ServerURL);
+                httppost.setEntity(new UrlEncodedFormEntity(NameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                is = entity.getContent();
+                Log.e("pass 1", "konekcija uspješna ");
+
+            } catch (Exception e) {
+
+                Log.e("Fail 1", e.toString());
+                Toast.makeText(getApplicationContext(), "Pogrešna IP Adresa",
+                        Toast.LENGTH_LONG).show();
+
+            }
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+                StringBuilder sb = new StringBuilder();
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line);
+                }
+                is.close();
+                result = sb.toString();
+                Log.e("pass 2", "konekcija uspješna ");
+
+            } catch (Exception e) {
+
+                Log.e("Fail 2", e.toString());
+            }
+
+            return null;
+
+        }
+
+        protected void onPostExecute(Void v) {
+
+
+            try {
+                //Skripta je ubacila korisnika u bazu podataka
+                //vraća se pomoću JSON-a rezultat 1 ako je sve prošlo ok ili 0 ako je registracija bila neuspješna
+                JSONObject json_data = new JSONObject(result);
+                code = (json_data.getInt("success"));
+
+
+                if (code == 0) {
+                    Toast.makeText(UserActivity.this, json_data.getString("message") , Toast.LENGTH_LONG).show();
+
+                } else {
+                    Toast.makeText(UserActivity.this, json_data.getString("message"), Toast.LENGTH_LONG).show();
+                    finish();
+
+                    startActivity(new Intent(UserActivity.this, PictureActivity.class));
+
+                }
+
+
+
+            } catch (Exception e) {
+
+                Log.e("log_tag", "Error parsing data " + e.toString());
+            }
+        }
+    }
+
+
+//___________________________________________
 
     //Checks if the network adapter is enabled
     private boolean isNetworkAvailable() {
---
+++ php_scripts/add_user.php
@@ -0,0 +1,53 @@
+<?php
+ 
+/*
+ * Following code will create a new product row
+ * All product details are read from HTTP Post Request
+ */
+ 
+// array for JSON response
+$response = array();
+ 
+// check for required fields
+if (isset($_REQUEST['name']) && isset($_REQUEST['age']) && isset($_REQUEST['sex'])) {
+ 
+    $name = $_REQUEST['name'];
+    $age = $_REQUEST['age'];
+    $sex = $_REQUEST['sex'];
+	
+	$id_phone = "proba1166227337";
+ 
+    // include db connect class
+    require_once __DIR__ . '/db_connect.php';
+ 
+    // connecting to db
+    $db = new DB_CONNECT();
+ 
+    // mysql inserting a new row
+    $result = mysql_query("INSERT INTO person(name, age, gender, id_phone, date) VALUES('$name', '$age', '$sex', '$id_phone', NOW())");
+ 
+    // check if row inserted or not
+    if ($result) {
+        // successfully inserted into database
+        $response["success"] = 1;
+        $response["message"] = "Product successfully created.";
+ 
+        // echoing JSON response
+        echo json_encode($response);
+    } else {
+        // failed to insert row
+        $response["success"] = 0;
+        $response["message"] = "Oops! An error occurred.";
+ 
+        // echoing JSON response
+        echo json_encode($response);
+    }
+} else {
+    // required field is missing
+    $response["success"] = 0;
+    $response["message"] = "Required field(s) is missing";
+ 
+    // echoing JSON response
+    echo json_encode($response);
+}
+?>
